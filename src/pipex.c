5d1c32f0e636334d5c75526334486359eeeda3e0
Ross Hvidsten <rhvidste@student.hive.fi>
2025-01-10 19:12:09 +0200

    todays progress

---
+++ src/pipex.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 19:08:51 by rhvidste          #+#    #+#             */
+/*   Updated: 2025/01/10 19:11:53 by rhvidste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	child_1_process(char **argv, char **envp, int *fd)
+{
+
+}
+
+void	child_2_process(char **argv, char **envp, int *fd)
+{
+	
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	pid_t	child1;
+	pid_t	child2;
+}
